75f18f94af0216283b284c4f9db37f0c85689749
Irina Dashkouskaya <63751849+irinadash@users.noreply.github.com>
2022-07-10 23:52:51 +0300

    Update HW1_extraJS.js

--- HW1_extraJS.js
+++ HW1_extraJS.js
@@ -3,14 +3,14 @@
 1*:
 Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
 */
-let age_2 = 20;
-let age_3 = 63;
+let age_2 = 18;
+let age_3 = 60;
 const checkAge = function(age) {
 if (age < age_2) {
       console.log("You don’t have access cause your age is " + age + " It’s less then " + age_2)
       } else {
           if ((age >= age_2) && (age < age_3)) {
-                  console.log("Welcome  ! ")
+                  console.log("Welcome!")
             } else {
                 if (age > age_3) {
                      console.log("Keep calm and look Culture channel")
